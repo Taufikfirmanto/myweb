f5be1c5318c950fd24b281a40af4df7e82f894b9
Taufikfirmanto <putumbahkasti@gmail.com>
2026-02-23 22:32:02 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,22 @@
-function toggleMenu() {
-    document.getElementById("menu").classList.toggle("show");
+function openLogin() {
+  document.getElementById("loginModal").style.display = "flex";
+}
+
+function closeLogin() {
+  document.getElementById("loginModal").style.display = "none";
 }
 
 function login() {
-    let user = document.getElementById("username").value;
-    let pass = document.getElementById("password").value;
-    let hasil = document.getElementById("hasil");
+  let user = document.getElementById("username").value;
+  let pass = document.getElementById("password").value;
+  let hasil = document.getElementById("hasil");
 
-    if (user && pass) {
-        localStorage.setItem("user", user);
-        hasil.style.color = "green";
-        hasil.innerHTML = "Login berhasil!";
-        tampilkanUser();
-    } else {
-        hasil.style.color = "red";
-        hasil.innerHTML = "Isi username & password!";
-    }
+  if (user && pass) {
+    hasil.style.color = "green";
+    hasil.innerHTML = "Login berhasil!";
+    localStorage.setItem("user", user);
+  } else {
+    hasil.style.color = "red";
+    hasil.innerHTML = "Isi semua field!";
+  }
 }
-
-function tampilkanUser() {
-    let user = localStorage.getItem("user");
-    if (user) {
-        document.getElementById("judul").innerHTML = "Halo, " + user;
-        document.getElementById("loginBox").style.display = "none";
-    }
-}
-
-function logout() {
-    localStorage.removeItem("user");
-    location.reload();
-}
-
-tampilkanUser();
